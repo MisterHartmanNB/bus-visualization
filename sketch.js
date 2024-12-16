25dbfc7640a7e9eea373889be5d782267044a485
MisterHartmanNB <john.hartman@newbremenschools.org>
2024-12-16 14:26:18 -0500

    Added ;buses, started the text to go with each visualization

--- sketch.js
+++ sketch.js
@@ -36,15 +36,21 @@ class Bus {
   }
 }
 
-var bus1 = new Bus(300,20);
-var bus2 = new Bus(100,60);
-var bus3 = new Bus(300,100);
+//Offset bus 1,2,3,4 by a variable to make room for text labelling each one
+var offset = 50;
+var bus1 = new Bus(300,20+offset);
+var bus2 = new Bus(100,60+offset*2);
+var bus3 = new Bus(100,140+offset*3);
+var bus4 = new Bus(300,190+offset*4);
+
+
 
 function setup() {
-  createCanvas(400,400);
+  createCanvas(400,500);
   bus1.dx = -2;
   bus2.dx = 2;
-  bus3.dx = -2;
+  bus3.dx = 2;
+  bus4.dx = -2;
 }
 
 function draw() {
@@ -54,7 +60,7 @@ function draw() {
   noFill();
   stroke('white');
   strokeWeight(5);
-  rect(100-bus1.width/2,20-bus1.height/2,bus1.width,bus1.height);
+  rect(100-bus1.width/2,20-bus1.height/2+offset,bus1.width,bus1.height);
 
   bus1.draw();
 
@@ -72,7 +78,7 @@ function draw() {
   noFill();
   stroke('white');
   strokeWeight(5);
-  rect(300-bus2.width/2,60-bus2.height/2,bus2.width,bus2.height);
+  rect(300-bus2.width/2,60-bus2.height/2+2*offset,bus2.width,bus2.height);
 
   if(bus2.x>=300) {
     bus2.dx = 0;
@@ -86,18 +92,78 @@ function draw() {
 
   bus2.draw();
 
+
+  //Bus 3, pulling into a parallel spot
   noFill();
   stroke('white');
   strokeWeight(5);
-  rect(100-bus3.width/2,100,bus3.width,bus3.height);
+  rect(300-bus3.width/2,100+3*offset,bus3.width,bus3.height);
 
-  if(bus3.x<=100) {
+  if(bus3.x>=300) {
     bus3.dx = 0;
     bus3.frame++;
     if(bus3.frame==30) {
-      bus3.x = 300;
+      bus3.x = 100;
+      bus3.y = 140+3*offset;
       bus3.frame = 0;
-      bus3.dx = -2;
+      bus3.dx = 2;
     }
   }
+
+  if(bus3.x>=150&&bus3.x<=250) {
+    bus3.dy = -0.3
+  } else {
+    bus3.dy = 0;
+  }
+
+  if(bus3.x>=175&&bus3.x<=225) {
+    bus3.dy = -0.9;
+  }
+
+  if(bus3.x>=140&&bus3.x<=165) {
+    bus3.rotation -= 0.018;
+  } else if(bus3.x>=210&&bus3.x<=235) {
+    bus3.rotation += 0.018;
+  }
+
+  bus3.draw();
+
+  //Bus 4, pulling out of the same spot
+  noFill();
+  stroke('white');
+  strokeWeight(5);
+  rect(300-bus4.width/2,180+4*offset,bus3.width,bus3.height);
+  
+  if(bus4.x<=100) {
+    bus4.dx = 0;
+    bus4.frame++;
+    if(bus4.frame==30) {
+      bus4.x = 300;
+      bus4.y = 190+4*offset;
+      bus4.frame = 0;
+      bus4.dx = -2;
+    }
+  }
+
+  if(bus4.x>=150&&bus4.x<=250) {
+    bus4.dy = 0.3
+  } else {
+    bus4.dy = 0;
+  }
+
+  if(bus4.x>=175&&bus4.x<=225) {
+    bus4.dy = 0.9;
+  }
+
+  if(bus4.x>=170&&bus4.x<=195) {
+    bus4.rotation += 0.018;
+  } else if(bus4.x>=213&&bus4.x<=238) {
+    bus4.rotation -= 0.018;
+  }
+  
+  bus4.draw();
+
+  //Write text to describe the directions for the driver in each situation
+  fill('black');
+  text("Step 1: Forward Stop - Drive forward through the alley and stop with the\nfront most part of the vehicle in the box without going past the front of\nthe alley", 10, offset/2);
 }
